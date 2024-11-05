92b407ecc53605b6fa977b34fcf0c927c09e9433
Bohong Sun <u7546803@anu.edu.au>
2024-11-05 21:41:53 +1100

    Done entityframework

---
+++ BookManagementSystem/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using BookManagementSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            return Ok(new List<Book>());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            return Ok(new Book());
+        }
+    }
+}
---
+++ BookManagementSystem/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookManagementSystem.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
